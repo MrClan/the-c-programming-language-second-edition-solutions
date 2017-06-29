4837d095db31d4034b5ca2ce3da30597a92a737c
MrClan_on_ElementaryOS <justmepratik@gmail.com>
2017-06-29 13:24:24 +0545

    Fixed Logic Error to get correct answer for hex numbers with alphabets

--- Chapter2/Exercise 2-03/htoi.c
+++ Chapter2/Exercise 2-03/htoi.c
@@ -1,3 +1,4 @@
+
 /**
  * Author: Jeremy Yu <ccpalettes@gmail.com>
  * 
@@ -42,19 +43,21 @@ unsigned long htoi(char s[])
     int i, c;
 
     n = 0;
-    for (i = 0; s[i] != '\0'; ++i) {
+    for (i = 0; s[i] != '\0'; ++i)
+    {
         c = s[i];
-        if (i == 0 && c == '0' && (s[1] == 'x' || s[1] == 'X')) {
+        if (i == 0 && c == '0' && (s[1] == 'x' || s[1] == 'X'))
+        {
             i = 1;
             continue;
-    }
-    n *= 16;
-    if (c >= '0' && c <= '9')
-        n += c - '0';
-    else if (c >= 'a' && c <= 'f')
-        n += c - 'a';
-    else if (c >= 'A' && c <= 'F')
-        n += c - 'A';
+        }
+        n *= 16;
+        if (c >= '0' && c <= '9')
+            n += c - '0';
+        else if (c >= 'a' && c <= 'f')
+            n += 10 + (c - 'a');
+        else if (c >= 'A' && c <= 'F')
+            n += 10 + (c - 'A');
     }
 
     return n;
